b0c799722e4af2936b872bbb33c6799950bc9e42
Yamil R. Velez <52504034+yrvelez@users.noreply.github.com>
2024-04-03 00:22:21 +0200

    Update functions.R
    Adding support for Claude 3 models

--- R/functions.R
+++ R/functions.R
@@ -1,43 +1,53 @@
 #' Interact with the Anthropics Claude API
 #'
 #' @param api_key Your API key for authentication.
-#' @param prompt The prompt you want Claude to complete.
-#' @param model The model to use for the request.
-#' @param max_tokens A maximum number of tokens to generate before stopping. Default = 100.
+#' @param prompt A string vector for Claude-2, or a list for Claude-3 specifying the input for the model.
+#' @param model The model to use for the request. Default is the latest Claude-3 model.
+#' @param max_tokens A maximum number of tokens to generate before stopping.
 #' @param stop_sequences (Optional) A list of strings upon which to stop generating.
 #' @param temperature (Optional) Amount of randomness injected into the response.
 #' @param top_k (Optional) Only sample from the top K options for each subsequent token.
 #' @param top_p (Optional) Does nucleus sampling.
 #' @return The resulting completion up to and excluding the stop sequences.
 #' @export
-claudeR <- function(prompt, model = "claude-2.1", max_tokens = 100,
-                                 stop_sequences = '\n\nHuman: ',
-                                 temperature = .7, top_k = -1, top_p = -1,
-                                 api_key = NULL) {
+claudeR <- function(prompt, model = "claude-3-opus-20240229", max_tokens = 100,
+                    stop_sequences = NULL,
+                    temperature = .7, top_k = -1, top_p = -1,
+                    api_key = NULL) {
+
   # Load required libraries
   library(httr)
   library(jsonlite)
 
+  if (grepl("claude-3", model) && !is.list(prompt)) {
+    stop("Claude-3 requires the input in a list format, e.g., list(list(role = \"user\", content = \"What is the capital of France?\"))")
+  }
+
   # Check if the API key is provided or available in the environment
   if (is.null(api_key)) {
-    api_key <- paste(Sys.getenv("ANTHROPIC_API_KEY"))
+    api_key <- Sys.getenv("ANTHROPIC_API_KEY")
     if (api_key == "") {
       stop("Please provide an API key or set it as the ANTHROPIC_API_KEY environment variable.")
     }
   }
 
-  # Set up the API request
-  url <- "https://api.anthropic.com/v1/complete"
-  headers <- add_headers(
-    "X-API-Key" = api_key,
-    "Content-Type" = "application/json",
-    "anthropic-version" = "2023-06-01"
-  )
+  # Set up conditions for claude-2
+  if (grepl("claude-2", model)) {
 
-  # Build the prompt with the User/Assistant convention
-  prompt <- paste0("\n\nHuman: ", prompt, "\n\nAssistant: ")
+    url <- "https://api.anthropic.com/v1/complete"
+    headers <- add_headers(
+      "X-API-Key" = api_key,
+      "Content-Type" = "application/json",
+      "anthropic-version" = "2023-06-01"
+    )
 
-  body <- paste0('{
+    # Build the prompt with the User/Assistant convention
+    prompt <- paste0("\n\nHuman: ", prompt, "\n\nAssistant: ")
+
+    # Stop sequences
+    stop_sequences = '\n\nHuman: '
+
+    body <- paste0('{
   "prompt": "', gsub("\n", "\\\\n", prompt), '",
   "model": "', model, '",
   "max_tokens_to_sample": ', max_tokens, ',
@@ -47,6 +57,48 @@ claudeR <- function(prompt, model = "claude-2.1", max_tokens = 100,
   "top_p": ', top_p, '
 }')
 
+    # Send the API request
+    response <- POST(url, headers, body = body)
+
+    # Check if the request was successful
+    if (http_status(response)$category == "Success") {
+      # Parse the JSON response
+      result <- fromJSON(content(response, "text", encoding = "UTF-8"))
+      return(trimws(result$completion))
+    } else {
+      # Print the error message
+      warning(paste("API request failed with status", http_status(response)$message))
+      cat(api_key)
+      cat("Error details:\n", content(response, "text", encoding = "UTF-8"), "\n")
+      return(NULL)
+    }
+
+  }
+
+  # Set up the API request
+  url <- "https://api.anthropic.com/v1/messages"
+
+  headers <- add_headers(
+    "x-api-key" = api_key,
+    "anthropic-version" = "2023-06-01",
+    "Content-Type" = "application/json"
+  )
+
+  # Prepare the messages list
+  message_list <- lapply(prompt, function(msg) {
+    list(role = msg$role, content = msg$content)
+  })
+
+  # Construct the body of the request
+  body <- toJSON(list(
+    model = model,
+    max_tokens = max_tokens,
+    temperature = temperature,
+    top_k = top_k,
+    top_p = top_p,
+    messages = message_list
+  ), auto_unbox = TRUE)
+
   # Send the API request
   response <- POST(url, headers, body = body)
 
@@ -54,13 +106,11 @@ claudeR <- function(prompt, model = "claude-2.1", max_tokens = 100,
   if (http_status(response)$category == "Success") {
     # Parse the JSON response
     result <- fromJSON(content(response, "text", encoding = "UTF-8"))
-    return(trimws(result$completion))
+    return(result$content$text)
   } else {
     # Print the error message
     warning(paste("API request failed with status", http_status(response)$message))
-    cat(api_key)
     cat("Error details:\n", content(response, "text", encoding = "UTF-8"), "\n")
     return(NULL)
   }
-
 }
